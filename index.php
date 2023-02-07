077ea79da921d4af6b71f363ec28775c49b6e31c
azxki <azxki01@gmail.com>
2023-02-07 14:12:00 +0100

    exo

--- index.php
+++ index.php
@@ -1,28 +1,24 @@
 <?php
-/**
- * 1. Le dossier SQL contient l'export de ma table user.
- * 2. Trouvez comment importer cette table dans une des bases de données que vous avez créées, si vous le souhaitez vous pouvez en créer une nouvelle pour cet exercice.
- * 3. Assurez vous que les données soient bien présentes dans la table.
- * 4. Créez votre objet de connexion à la base de données comme nous l'avons vu
- * 5. Insérez un nouvel utilisateur dans la base de données user
- * 6. Modifiez cet utilisateur directement après avoir envoyé les données ( on imagine que vous vous êtes trompé )
- */
 
-// TODO Votre code ici.
+$username = 'root';
+$password = '';
+
+$file = file_get_contents('./classes/SQL/user.sql');
+
 try {
-    ...
+    $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $db->lastInsertId();
+
+    $result = $db->exec($file);
+    if($result) {
+        $id = $db->lastInsertId();
+    }
+
+
+    echo "requête validé !";
 }
-catch...
-
-
-
-/**
- * Théorie
- * --------
- * Pour obtenir l'ID du dernier élément inséré en base de données, vous pouvez utiliser la méthode: $bdd->lastInsertId()
- *
- * $result = $bdd->execute();
- * if($result) {
- *     $id = $bdd->lastInsertId();
- * }
- */
+catch (PDOException $exception) {
+    echo $exception->getMessage();
+}
